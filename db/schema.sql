85f3bc51c87d74b5a33e2084a5c5a41648e8c97c
Eric Cole <ecole052785@gmail.com>
2021-05-26 12:44:11 -0400

    some scaffolding completed

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS employees
+CREATE DATABASE employees
+
+USE employees
+
+--department table----------------------------------------------------------------
+CREATE TABLE department (
+    --Your code here----------------------------------------------------------------
+)
+CREATE TABLE role (
+    --Your code here----------------------------------------------------------------
+)
+CREATE TABLE employee (
+    --Your code here----------------------------------------------------------------
+)
